55b325ffed4bf8a8e98588d92b82d2572919057c
Freeha Anjum <freeha.anjum@ucalgary.ca>
2022-11-20 22:27:47 -0700

    initial setup of files, classes, and scenes

---
+++ src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import java.io.FileInputStream;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/ScheduleCheckerView.fxml"));
+			ScheduleCheckerController controller = (ScheduleCheckerController)loader.getController();
+			controller.applicationStage = primaryStage;
+			Scene scene = new Scene(root,400,400);
+			
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Schedule Checker");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
