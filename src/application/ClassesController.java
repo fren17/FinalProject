c15c0e06b45ac704c8d6f11abd3f75c42270a7f7
Freeha Anjum <freeha.anjum@ucalgary.ca>
2022-11-28 09:45:36 -0700

    adding button + new import for ActionEvent

--- src/application/ClassesController.java
+++ src/application/ClassesController.java
@@ -1,16 +1,26 @@
 package application;
 
-import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ClassesController {
+	private Stage primaryStage;
+	private Scene mainScene;
+	private Parent root;
 	
 	@FXML
 	public void doneCourses(ActionEvent event) throws IOException{
-		//take back to main page
+		Parent root = FXMLLoader.load(getClass().getResource("ClassesView.fxml"));
+		primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		mainScene = new Scene(root);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
 	}
 }
